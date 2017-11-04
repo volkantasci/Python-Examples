c223269bef313838901184d1db88aaa81feeee37
Volkan Taşcı <proprogramci@outlook.com>
2017-11-04 16:56:07 +0300

    hello_world.py dosyası düzenlendi

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,9 @@
 print("Merhaba Dünya!")
+"""
+print fonksiyonunu kullanarak basit bir şekilde ekrana 'Merhaba 
+Dünya' yazdırdık.
+
+print fonksiyonunun end parametresinden dolayı da fonksiyon sonlanmadan 
+önce alt satıra indi.
+"""
+
